42eb4174197a20e11c8ec6077f80c86283d26304
jinyu <jinyu1@zhaohu365.cn>
2023-04-20 16:27:54 +0800

    修复编译bug

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,39 +1,63 @@
-require("@nomiclabs/hardhat-waffle");
-require('hardhat-abi-exporter');
+require('@nomiclabs/hardhat-waffle')
+require('hardhat-abi-exporter')
+let dotenv = require('dotenv')
+dotenv.config({ path: './.env' })
 
-task("accounts", "Prints the list of accounts", async() => {
-    const accounts = await ethers.getSigners();
+task('accounts', 'Prints the list of accounts', async () => {
+  const accounts = await ethers.getSigners()
 
-    for (const account of accounts) {
-        console.log(account.address);
-    }
-});
+  for (const account of accounts) {
+    console.log(account.address)
+  }
+})
+
+const account = process.env.GOERLI_PRIVATE_KEY
+const alchemyApiKey = process.env.ALCHEMY_API_KEY
+const alchemySepoliaApiKey = process.env.ALCHEMY_API_KEY
+const etherscanKey = process.env.ETHERSCAN_KEY
+const mumbaiKey = process.env.MUMBAI_KEY
+const mumbaiScanApiKey = process.env.MUMBAI_POLYGON_API_KEY
 
 module.exports = {
-    solidity: "0.5.16",
-    // solidity: {
-    //     version: "0.5.16",
-    //     settings: {
-    //         optimizer: {
-    //             enabled: true,
-    //             runs: 200
-    //         }
-    //     }
-    // },
+  solidity: '0.5.16',
+  // solidity: {
+  //     version: "0.5.16",
+  //     settings: {
+  //         optimizer: {
+  //             enabled: true,
+  //             runs: 200
+  //         }
+  //     }
+  // },
 
-    abiExporter: {
-        path: './deployments/abi',
-        clear: true,
-        flat: true,
-        only: [],
-        spacing: 2,
-        pretty: true,
+  abiExporter: {
+    path: './deployments/abi',
+    clear: true,
+    flat: true,
+    only: [],
+    spacing: 2,
+    pretty: true
+  },
+  networks: {
+    hardhat: {},
+    dev: {
+      url: 'http://127.0.0.1:8545',
+      chainId: 31337
     },
-
-    networks: {
-        dev: {
-            url: "http://127.0.0.1:8545",
-            chainId: 31337,
-        }
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${alchemyApiKey}`,
+      accounts: [account]
+    },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${alchemySepoliaApiKey}`,
+      accounts: [account]
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${alchemySepoliaApiKey}`,
+      accounts: [account]
     }
-};
+  },
+  etherscan: {
+    apiKey: `${etherscanKey}`
+  },
+}
